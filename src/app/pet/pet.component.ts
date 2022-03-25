ce7d07011eea303a0f0d6a7eabbd237fa801f231
trieu.long <long.trieu.hr@gmail.com>
2022-03-25 10:22:30 +0700

    pet info

---
+++ src/app/pet/pet.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Pet } from '../pet';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.css']
+})
+export class PetComponent implements OnInit {
+  pet: Pet = {
+    name: 'puppie',
+    image: 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg'
+  };
+  petName = 'puppie';
+  petImage = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
+  constructor() { }
+
+  updateName(name) {
+    this.petName = name;
+  }
+
+  updateImage(image) {
+    this.petImage = image;
+  }
+
+
+  ngOnInit() {
+  }
+
+}
